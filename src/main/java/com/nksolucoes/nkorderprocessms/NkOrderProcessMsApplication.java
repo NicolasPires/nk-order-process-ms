3ca2b4d12af74b4ed42746f1a4a854ebf09a1b6a
Nicolas Pires <ntconsult.pires@agi.com.br>
2024-12-20 15:39:39 -0300

    initial commit

---
+++ src/main/java/com/nksolucoes/nkorderprocessms/NkOrderProcessMsApplication.java
@@ -0,0 +1,13 @@
+package com.nksolucoes.nkorderprocessms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NkOrderProcessMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NkOrderProcessMsApplication.class, args);
+	}
+
+}
